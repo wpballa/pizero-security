53faea9b3e2e26275ae35503a5d02d83c3999042
Bill Ballard <wpballa1@gmail.com>
2019-04-01 17:31:29 -0700

    Update tempupdate.sh
    Modification to fix crontab issue during daylight savings time changeover.

--- tempupdate.sh
+++ tempupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-STR=$(date -d "last month" +"%y-%m")
+STR=$(date -d "last week" +"%y-%m")
 mv /home/pi/temp.log /home/pi/$STR-temp.log
 heirloom-mailx -a /home/pi/$STR-temp.log -s "monthly temperature log" youremail@gmail.com
 #heirloom-mailx -a /home/pi/$STR-temp.log -s "monthly temperature log" another-email@gmail.com
