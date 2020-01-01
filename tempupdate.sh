05d86a06891fe1325e07686aee04ecf7ef7dc8b2
Bill Ballard <wpballa1@gmail.com>
2020-01-01 09:44:37 -0800

    Update tempupdate.sh
    Forgot to update to s-nail

--- tempupdate.sh
+++ tempupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 STR=$(date -d "last week" +"%y-%m")
 mv /home/pi/temp.log /home/pi/$STR-temp.log
-heirloom-mailx -a /home/pi/$STR-temp.log -s "monthly temperature log" youremail@gmail.com
-#heirloom-mailx -a /home/pi/$STR-temp.log -s "monthly temperature log" another-email@gmail.com
+echo "monthly temperature log" | s-nail -a /home/pi/$STR-temp.log -s "monthly temperature log" youremail@gmail.com
+#echo "monthly temperature log" | s-nail -a /home/pi/$STR-temp.log -s "monthly temperature log" another-email@gmail.com
 
